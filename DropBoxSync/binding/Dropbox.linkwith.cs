875a841b0e5121b64338e58d707ea0b7a71a423d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-23 03:04:58 +0100

    [DropBoxSync] Enable smart linking, add LinkerSafe attribute, set deployment target to 5.0 to avoid linker errors and update .gitignores.

--- DropBoxSync/binding/Dropbox.linkwith.cs
+++ DropBoxSync/binding/Dropbox.linkwith.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("Dropbox.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CFNetwork Security SystemConfiguration QuartzCore")]
+[assembly: LinkWith ("Dropbox.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = true, Frameworks = "CFNetwork Security SystemConfiguration QuartzCore")]
+[assembly: LinkerSafe]
