62e76b4dceff768c53b9303dda963da826f4dab0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-23 02:18:59 +0100

    [BeeblexSDK] Add LinkerSafe attribute and update .gitignore.

--- BeeblexSDK/binding/AssemblyInfo.cs
+++ BeeblexSDK/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libBeeblex-SDK.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks="SystemConfiguration Security StoreKit")]
+[assembly: LinkWith ("libBeeblex-SDK.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = true, Frameworks="SystemConfiguration Security StoreKit")]
+[assembly: LinkerSafe]
