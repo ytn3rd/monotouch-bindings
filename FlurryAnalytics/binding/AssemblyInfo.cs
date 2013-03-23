561480d53908cdd11e0d062617f4eeb2c29c1dbb
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-23 03:21:04 +0100

    [FlurryAnalytics] Enable smart linking, add LinkerSafe attribute and update gitignores.

--- FlurryAnalytics/binding/AssemblyInfo.cs
+++ FlurryAnalytics/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libFlurry.a", LinkTarget.Simulator | LinkTarget.ArmV7,Frameworks = "SystemConfiguration", ForceLoad = true)]
+[assembly: LinkWith ("libFlurry.a", LinkTarget.Simulator | LinkTarget.ArmV7,Frameworks = "SystemConfiguration", ForceLoad = true, SmartLink = true)]
+[assembly: LinkerSafe]
