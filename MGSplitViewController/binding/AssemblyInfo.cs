35c62224c9199e1074aacc09f4f710314a57177e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-27 00:49:31 +0100

    [MGSplitViewController] Add LinkerSafe attribute and enable smart linking.

--- MGSplitViewController/binding/AssemblyInfo.cs
+++ MGSplitViewController/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libMGSplitViewControllerSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "CoreGraphics", ForceLoad = true)]
+[assembly: LinkWith ("libMGSplitViewControllerSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "CoreGraphics", ForceLoad = true, SmartLink = true)]
+[assembly: LinkerSafe]
