be538305da3462db716817c3fea3bc2a895aeb66
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-26 16:27:00 +0100

    [GCDiscreetNotification] Enable smart linking, add LinkerSafe attribute and ignore built files.

--- GCDiscreetNotification/binding/AssemblyInfo.cs
+++ GCDiscreetNotification/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libGCDiscreetNotificationView.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics")]
+[assembly: LinkWith ("libGCDiscreetNotificationView.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = true, Frameworks = "CoreGraphics")]
+[assembly: LinkerSafe]
