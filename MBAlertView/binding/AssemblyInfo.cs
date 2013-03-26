fc47953b3e13c8189640cd2c63b284efc9a093d7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-27 00:36:09 +0100

    [MBAlertView] Add LinkerSafe attribute and update gitignore.
    Do not enable smart linking, it makes the app fail on startup.

--- MBAlertView/binding/AssemblyInfo.cs
+++ MBAlertView/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libMBAlertView.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics QuartzCore")]
+[assembly: LinkWith ("libMBAlertView.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = false, Frameworks = "CoreGraphics QuartzCore")]
+[assembly: LinkerSafe]
