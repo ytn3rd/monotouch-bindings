447388f709a932a10cb8b53cd2459ea5d3b9bb21
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-27 01:06:14 +0100

    [MagTek.iDynamo] Add LinkerSafe attribute and enable smart linking.

--- MagTek.iDynamo/binding/AssemblyInfo.cs
+++ MagTek.iDynamo/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libMTSCRA.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Thumb, "-lc++", Frameworks = "AudioToolbox CoreGraphics ExternalAccessory", ForceLoad = true, IsCxx = true)]
+[assembly: LinkWith ("libMTSCRA.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Thumb, "-lc++", Frameworks = "AudioToolbox CoreGraphics ExternalAccessory", ForceLoad = true, SmartLink = true, IsCxx = true)]
+[assembly: LinkerSafe]
