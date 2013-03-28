e5b6df99f4f51015a8b1efa2c42689c5288e6533
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-03-28 02:18:31 +0100

    [Route-Me] Enable smart linking and add LinkerSafe attribute.

--- Route-Me/binding/libMapView.linkwith.cs
+++ Route-Me/binding/libMapView.linkwith.cs
@@ -1,6 +1,8 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
 [assembly: LinkWith ("libMapView.a", LinkTarget.ArmV7 | LinkTarget.Simulator, 
                      Frameworks = "CoreFoundation CoreLocation QuartzCore UIKit Foundation CoreGraphics", 
-                     LinkerFlags = "-lz -lsqlite3", ForceLoad = true, IsCxx = true)]
+                     LinkerFlags = "-lz -lsqlite3", ForceLoad = true, SmartLink = true, IsCxx = true)]
+[assembly: LinkerSafe]
