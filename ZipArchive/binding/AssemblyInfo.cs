bb9c19522900a3b1e28a5bd319b55a448864d958
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-04-04 01:15:19 +0200

    [ZipArchive] Add Protocol and LinkerSafe attributes, enable smart linking and ignore some files.

--- ZipArchive/binding/AssemblyInfo.cs
+++ ZipArchive/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libziparchive.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, IsCxx=true)]
+[assembly: LinkWith ("libziparchive.a", LinkTarget.ArmV7s | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, SmartLink = true, IsCxx=true)]
+[assembly: LinkerSafe]
