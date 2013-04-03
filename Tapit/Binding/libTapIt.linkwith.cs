970e0821f2e9726e7760586126a9086e9db37240
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-04-03 23:58:26 +0200

    [TapIt] Add Protocol and LinkerSafe attributes, improve Makefile.
    Smart linking cannot be enabled, it throws a selector not found exception at startup.

--- Tapit/Binding/libTapIt.linkwith.cs
+++ Tapit/Binding/libTapIt.linkwith.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libTapIt.a", LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith ("libTapIt.a", LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, SmartLink = false, Frameworks = "CoreGraphics CoreTelephony QuartzCore SystemConfiguration")]
+[assembly: LinkerSafe]
