376ff39473cba5a184ff8b9fe3613fffd54b508d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-04-06 04:01:35 +0200

    [Couchbase] Enable smart linking and add a LinkerSafe attribute.

--- Couchbase/binding/libCouchCocoa.linkwith.cs
+++ Couchbase/binding/libCouchCocoa.linkwith.cs
@@ -1,5 +1,5 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libCouchCocoa.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
-[assembly: LinkWith ("libTouchDB.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true)]
+[assembly: LinkWith ("libCouchCocoa.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, SmartLink = true)]
+[assembly: LinkWith ("libTouchDB.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, SmartLink = true)]
--- Couchbase/binding/libTouchDB.linkwith.cs
+++ Couchbase/binding/libTouchDB.linkwith.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libTouchDB.a", LinkTarget.ArmV7 | LinkTarget.Simulator, LinkerFlags = "-lz -lsqlite3", Frameworks = "SystemConfiguration CFNetwork", ForceLoad=true)]
+[assembly: LinkWith ("libTouchDB.a", LinkTarget.ArmV7 | LinkTarget.Simulator, LinkerFlags = "-lz -lsqlite3", Frameworks = "SystemConfiguration CFNetwork", ForceLoad = true, SmartLink = true)]
+[assembly: LinkerSafe]
