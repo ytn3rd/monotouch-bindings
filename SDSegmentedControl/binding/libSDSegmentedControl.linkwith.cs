f0d8fe43b3dab364e9e333c26b25a60fc2dd7fbb
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-04-08 12:23:24 +0200

    [SDSegmentedControl] Add LinkerSafe attribute and enable smart linking.

--- SDSegmentedControl/binding/libSDSegmentedControl.linkwith.cs
+++ SDSegmentedControl/binding/libSDSegmentedControl.linkwith.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libSDSegmentedControl.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.ArmV7s, Frameworks = "QuartzCore CoreGraphics", ForceLoad = true)]
+[assembly: LinkWith ("libSDSegmentedControl.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.ArmV7s, Frameworks = "QuartzCore CoreGraphics", ForceLoad = true, SmartLink = true)]
+[assembly: LinkerSafe]
