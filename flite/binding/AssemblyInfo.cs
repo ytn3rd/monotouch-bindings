dd6e1f70b89b100e5dc752ef95bfb2e9dbad5e4f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-04-08 12:47:37 +0200

    [flite] Add LinkerSafe to AssemblyInfo.cs.

--- flite/binding/AssemblyInfo.cs
+++ flite/binding/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libflite.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true,IsCxx = true)]
+[assembly: LinkWith ("libflite.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true, SmartLink = true, IsCxx = true)]
+[assembly: LinkerSafe]
