cef512fbdcafa942e1b2f5a12629502273db1135
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-04-08 13:54:50 +0200

    [RedLaser] Add Protocol and LinkerSafe attributes, enable smart linking and managed linker.

--- RedLaser/binding/AssemblyInfo.cs
+++ RedLaser/binding/AssemblyInfo.cs
@@ -1,4 +1,8 @@
 using System;
 using MonoTouch.ObjCRuntime;
+using MonoTouch.Foundation;
 
-[assembly: LinkWith ("libRedLaserSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, Frameworks = "AudioToolbox AVFoundation CFNetwork CoreMedia CoreVideo OpenGLES Security SystemConfiguration CoreLocation", ForceLoad = true, IsCxx = true, LinkerFlags = "-lstdc++")]
+[assembly: LinkWith ("libRedLaserSDK.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, 
+                     Frameworks = "AudioToolbox AVFoundation CFNetwork CoreMedia CoreVideo OpenGLES Security SystemConfiguration CoreLocation", 
+                     ForceLoad = true, SmartLink = true, IsCxx = true, LinkerFlags = "-lstdc++")]
+[assembly: LinkerSafe]
