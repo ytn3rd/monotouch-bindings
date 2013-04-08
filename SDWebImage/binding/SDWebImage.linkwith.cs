3222226cdaf1a74e8513d260cb5191f246cfb3d7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2013-04-08 13:17:12 +0200

    [SDWebImage] Disable smart linking, the native library requires -force_load.

--- SDWebImage/binding/SDWebImage.linkwith.cs
+++ SDWebImage/binding/SDWebImage.linkwith.cs
@@ -1,5 +1,6 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libSDWebImage.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = true, Frameworks = "CoreGraphics ImageIO")]
+// libSDWebImage.a requires -force_load: http://stackoverflow.com/questions/8504953/sdwebimage-setimagewithurl-fails-when-used-to-set-image-of-a-uitableviewcell
+[assembly: LinkWith ("libSDWebImage.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, SmartLink = false, Frameworks = "CoreGraphics ImageIO")]
 // the LinkerSafe attribute is already in libarclite.linkwith.cs
